28ae128b620154eb8ef54f61e81dea552d312835
rob-ng15 <58272847+rob-ng15@users.noreply.github.com>
2020-08-30 07:58:21 +0100

    BLINKY
    Pulse the LED

---
+++ BLINKY/fomu_hacker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+rm build*
+
+silice -f frameworks/fomu.v $1 -o build.v
+
+yosys -D HACKER=1 -p 'synth_ice40 -top top -json build.json' build.v
+nextpnr-ice40 --up5k --package uwg30 --opt-timing --pcf pcf/fomu-hacker.pcf --json build.json --asc build.asc
+icepack build.asc build.bit
+icetime -d up5k -mtr build.rpt build.asc
+cp build.bit build.dfu
+dfu-suffix -v 1209 -p 70b1 -a build.dfu
