69f8b39a48c172d5f27ff9a6f6efd60d86a50e1d
rob-ng15 <58272847+rob-ng15@users.noreply.github.com>
2021-02-02 12:01:50 +0000

    Try compiling with -function-sections
    Can strip out unnecessary library functions and make programs smaller.

--- PAWS/SOFTWARE/strip-compile.sh
+++ PAWS/SOFTWARE/strip-compile.sh
@@ -14,11 +14,11 @@ NM="riscv64-elf-nm"
 RANLIB="riscv64-elf-ranlib"
 
 # Following based on FemtoRV compile scripts https://github.com/BrunoLevy/learn-fpga/tree/master/FemtoRV
-$ARCH-elf-gcc -fno-unroll-loops -O2 -fno-builtin -fno-pic -march=rv32imac -mabi=ilp32 -c -o build/libPAWS.o c/PAWSlibrary.c
+$ARCH-elf-gcc -ffunction-sections -fdata-sections -fno-unroll-loops -O2 -fno-builtin -fno-pic -march=rv32imac -mabi=ilp32 -c -o build/libPAWS.o c/PAWSlibrary.c
 $AR -cvq build/libPAWS.a build/libPAWS.o
-$ARCH-elf-gcc -fno-unroll-loops -O2 -fno-builtin -fno-pic -march=rv32imac -mabi=ilp32 -c -o build/code.o $1
+$ARCH-elf-gcc -ffunction-sections -fdata-sections -fno-unroll-loops -O2 -fno-builtin -fno-pic -march=rv32imac -mabi=ilp32 -c -o build/code.o $1
 $ARCH-elf-as -march=rv32imac -mabi=ilp32 -o build/crt0.o crt0.s
-$ARCH-elf-ld --as-needed -m elf32lriscv -b elf32-littleriscv -Tconfig_c_SDRAM.ld --no-relax -o build/code.elf build/code.o build/libPAWS.o
+$ARCH-elf-ld --as-needed --gc-sections -m elf32lriscv -b elf32-littleriscv -Tconfig_c_SDRAM.ld --no-relax -o build/code.elf build/code.o build/libPAWS.o
 
 $ARCH-elf-objcopy -O binary build/code.elf $2
 
